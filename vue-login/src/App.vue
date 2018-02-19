11b46694b0f05878144f9c4368aa352ed0ac0c79
Liu Cong <1434580150@qq.com>
2018-02-19 18:33:34 +0800

    add vuetify/materialicon

---
+++ vue-login/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>supervisor_accounts</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click="drawer=!drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return {
+      drawer:false,
+    }
+  },
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
